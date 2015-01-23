6fea3d481a54b803dee2a4bb4309cc274bd13053
Ed-88 <eduard1988@i.ua>
2015-01-23 11:56:27 +0200

    commit

--- tmp/ads.sql
+++ tmp/ads.sql
@@ -1,6 +1,6 @@
 /*
-SQLyog Ultimate v11.52 (64 bit)
-MySQL - 5.5.40-0ubuntu0.14.04.1 : Database - ads_board
+SQLyog Community v11.52 (32 bit)
+MySQL - 5.5.41-0ubuntu0.14.04.1 : Database - ads_board
 *********************************************************************
 */
 
@@ -46,15 +46,16 @@ DROP TABLE IF EXISTS `categories`;
 
 CREATE TABLE `categories` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(16) DEFAULT NULL,
-  `description` varchar(32) DEFAULT NULL,
-  `pid` int(11) unsigned DEFAULT '0',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8;
+  `name` varchar(16) NOT NULL,
+  `description` varchar(32) NOT NULL,
+  `pid` int(11) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8;
 
 /*Data for the table `categories` */
 
-insert  into `categories`(`id`,`name`,`description`,`pid`) values (1,'asdasdas','asdasda',0),(16,'новая','категория',0),(17,'вввввв','фвыфывфывфыв',1),(18,'фывфыв','фывфывфыв',17),(19,'asdasdasdsad','asdsadasdasdasd',17),(20,'Отдых','Принадлежности для отдыха',0),(21,'ККККК','КККККК',20);
+insert  into `categories`(`id`,`name`,`description`,`pid`) values (1,'Техника','Техника',0),(16,'новая','категория',0),(17,'Планшеты','Планшеы',1),(18,'Телефоны','Мобильные телефоны',1),(19,'asdasdasdsad','asdsadasdasdasd',16),(20,'Отдых','Принадлежности для отдыха',0),(21,'Винчестеры','Жосткие диски',1),(22,'aaaaaaaaaaa','aaaaaaaaaaaaaa',20),(30,'11111','111111',20);
 
 /*Table structure for table `comments` */
 
@@ -72,11 +73,11 @@ CREATE TABLE `comments` (
   KEY `ad_id` (`ad_id`),
   CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
   CONSTRAINT `comments_ibfk_2` FOREIGN KEY (`ad_id`) REFERENCES `ads` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
 /*Data for the table `comments` */
 
-insert  into `comments`(`id`,`user_id`,`ad_id`,`text`,`date_create`,`status`) values (1,3,1,'Хороший товар, рэспект','2015-01-18 15:06:34',NULL),(2,3,2,'asdasdasdasdasd','2015-01-18 15:45:30',NULL);
+insert  into `comments`(`id`,`user_id`,`ad_id`,`text`,`date_create`,`status`) values (1,3,1,'Хороший товар, рэспект','2015-01-18 15:06:34',NULL),(2,3,2,'asdasdasdasdasd','2015-01-18 15:45:30',NULL),(3,2,2,'asdasdasdasdasdas','2015-01-19 17:51:42',NULL),(4,2,2,'asdasdasdadadadsasda','2015-01-19 17:51:47',NULL);
 
 /*Table structure for table `images` */
 
@@ -84,9 +85,11 @@ DROP TABLE IF EXISTS `images`;
 
 CREATE TABLE `images` (
   `id` int(11) NOT NULL,
-  `ads_id` int(11) DEFAULT NULL,
+  `ads_id` int(11) unsigned DEFAULT NULL,
   `name` varchar(64) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `ads_id` (`ads_id`),
+  CONSTRAINT `images_ibfk_1` FOREIGN KEY (`ads_id`) REFERENCES `ads` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 /*Data for the table `images` */
@@ -107,7 +110,7 @@ CREATE TABLE `payments` (
   KEY `plan_id` (`plan_id`),
   CONSTRAINT `payments_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
   CONSTRAINT `payments_ibfk_2` FOREIGN KEY (`plan_id`) REFERENCES `plans` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 /*Data for the table `payments` */
 
@@ -128,6 +131,59 @@ CREATE TABLE `plans` (
 /*Data for the table `plans` */
 
 insert  into `plans`(`id`,`name`,`price`,`count_ads`) values (1,'free',0,10),(2,'pro',99,1000),(3,'business',199,-1);
+
+/*Table structure for table `properties` */
+
+DROP TABLE IF EXISTS `properties`;
+
+CREATE TABLE `properties` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `params` varchar(256) DEFAULT NULL,
+  `type` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+
+/*Data for the table `properties` */
+
+insert  into `properties`(`id`,`name`,`params`,`type`) values (3,'Тип жесткого диска','[\"111\",\"asdasdad\",\"asdasd\",\"22\",\"pp\"]','select'),(4,'Форм-фактор','[\"3.5\",\"2.5\"]','radio'),(5,'Согласен высылать за границу','[\"apply\"]','checkbox'),(6,'Скорость передачи данных','[\"Mb/s\"]','textfield'),(7,'111','[\"111\",\"111\",\"111\"]','radio'),(8,'222','[\"222\",\"222\",\"999\"]','textbox');
+
+/*Table structure for table `property_ads` */
+
+DROP TABLE IF EXISTS `property_ads`;
+
+CREATE TABLE `property_ads` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `ads_id` int(11) unsigned DEFAULT NULL,
+  `property_id` int(11) unsigned DEFAULT NULL,
+  `value` varchar(64) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `ads_id` (`ads_id`),
+  KEY `property_id` (`property_id`),
+  CONSTRAINT `property_ads_ibfk_1` FOREIGN KEY (`ads_id`) REFERENCES `ads` (`id`),
+  CONSTRAINT `property_ads_ibfk_2` FOREIGN KEY (`property_id`) REFERENCES `properties` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `property_ads` */
+
+/*Table structure for table `property_cats` */
+
+DROP TABLE IF EXISTS `property_cats`;
+
+CREATE TABLE `property_cats` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `category_id` int(11) unsigned DEFAULT NULL,
+  `property_id` int(11) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `category_id` (`category_id`),
+  KEY `property_id` (`property_id`),
+  CONSTRAINT `property_cats_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`),
+  CONSTRAINT `property_cats_ibfk_2` FOREIGN KEY (`property_id`) REFERENCES `properties` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8;
+
+/*Data for the table `property_cats` */
+
+insert  into `property_cats`(`id`,`category_id`,`property_id`) values (2,21,4),(8,16,3),(9,16,4),(10,16,3),(11,16,4),(12,16,3),(13,16,3),(22,17,3),(24,22,4),(25,22,6),(26,22,7);
 
 /*Table structure for table `users` */
 
@@ -153,7 +209,7 @@ CREATE TABLE `users` (
 
 /*Data for the table `users` */
 
-insert  into `users`(`id`,`login`,`name`,`password`,`status`,`role`,`email`,`phone`,`salt`,`guid`,`date_create`,`plan_id`) values (2,'admin','admin','9cbf72dba5709b3298bbd2bbbf8b1e85','confirmed','admin','admin@ukr.net','+3806347531','54abac7224de2','yqd9bMeX9x','2015-01-06 11:35:46',1),(3,'vas','vas','60d137ddc301fe95265528419e33ac27','confirmed','user','vas@vas.com','123','54b5713a425ec','XGoKVwGkVF','2015-01-13 21:25:46',1);
+insert  into `users`(`id`,`login`,`name`,`password`,`status`,`role`,`email`,`phone`,`salt`,`guid`,`date_create`,`plan_id`) values (2,'admin','admin','9cbf72dba5709b3298bbd2bbbf8b1e85','confirmed','admin','admin@ukr.net','+3806347531','54abac7224de2','JxR5SNfio1','2015-01-06 11:35:46',1),(3,'vas','vas','60d137ddc301fe95265528419e33ac27','confirmed','user','vas@vas.com','123','54b5713a425ec','XGoKVwGkVF','2015-01-13 21:25:46',1);
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
