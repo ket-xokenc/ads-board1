ec2340f514435abea0209845846fc2d5a5e25bfc
alexandr <kvasenko@ukr.net>
2015-01-24 13:50:46 +0200

    add validation for comments

--- public/js/ajax.js
+++ public/js/ajax.js
@@ -31,6 +31,35 @@ $(function() {
 
     var working = false;
 
+    $('#addCommentContainer').on('submit', '#addCommentFormEnd', function (e) {
+        e.preventDefault();
+        if (working) return false;
+        var a;
+        a = this;
+
+        working = true;
+        $('#submit').val('send');
+        $('span.error').remove();
+
+        $.post('add-comment', $(this).serialize(), function (msg) {
+            working = false;
+            if (msg.status) {
+                $('#addNewComment').before(msg.html);
+                commentForm.remove();
+                var res = $(this).find('input').eq(1).attr('pid');
+                if(res ==  '0') {
+                    $('#addCommentContainer').siblings(":last").after($('addNewComment').content());
+                }
+            }
+            else {
+
+                $.each(msg.errors, function (k, v) {
+                    $('label[for=' + k + ']').append(' <span style="color:red" class="error">' + v + '</span>');
+                });
+            }
+        }, 'json');
+
+    });
 
     $('#addCommentContainer').on('submit', '#addCommentForm', function (e) {
         e.preventDefault();
@@ -46,11 +75,9 @@ $(function() {
             working = false;
             if (msg.status) {
                 $('#addCommentContainer').siblings(":last").after(msg.html);
-//                    $('#addNewComment').insertAfter($('.row').siblings(":last"));
                 commentForm.remove();
                 var res = $(this).find('input').eq(1).attr('pid');
                 if(res ==  '0') {
-                    $('addNewComment').show();
                     $('#addCommentContainer').siblings(":last").after($('addNewComment').content());
                 }
             }
@@ -71,9 +98,8 @@ $(function() {
         var current = $(this);
 
         commentForm = $('#addCommentContainer').clone(true, true);
-
         if ($(this).attr('id') == 'addNewComment') {
-            $('#addNewComment').hide();
+            commentForm.find('form').attr('id', 'addCommentFormEnd');
             commentForm.appendTo(current.parent());
         } else {
             commentForm.insertAfter(current.parent());
